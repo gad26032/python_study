ef5825e107f19c5105d386bac85d8aeefbcbbd07
gad26032 <gad26032@gmail.com>
2021-08-16 14:31:23 +0300

    add blanks task

--- lesson_8_9/app.py
+++ lesson_8_9/app.py
@@ -118,11 +118,17 @@ def update_blanks(blanks, letter, secret_word):
     pass
 
 
-
 # Сравнить каждую букву в secret_word с letter
 # Елси буквы равны то заменить символ под темже индексом в blanks
 # что и символ в secret_word буквой из letter
+# blanks = '_____'
+# letter = 'к'
+# secret_word = "акула"
+# в резултате blanks должен содержать "_к___"
 
+# 1. найти индекс искомой буквы в secret_wod (index)
+# 2. Заменить в blanks пробел под тем же индексом(index) на искомую букву(letter)
+# Пример blanks[:3] + "rsitenrti" + blanks[3:]
 
 
 def main():
